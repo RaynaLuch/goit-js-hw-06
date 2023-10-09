c2c3f7120e64b01b339a989b844d9775ce6babbc
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-10-09 21:39:40 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const myParent = document.querySelector("ul#ingredients");
+ingredients.forEach((elem) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = elem;
+  ingredient.classList.add("item");
+  myParent.append(ingredient);
+});
