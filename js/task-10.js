eed419d796a86d0a399cffad376d29cb2f2b22df
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-10-14 23:01:25 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+const divBoxes = document.querySelector("div#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    const divBox = document.createElement("div");
+    divBoxes.append(divBox);
+
+    divBox.style.height = size + "px";
+    divBox.style.width = size + "px";
+    divBox.style.backgroundColor = getRandomHexColor();
+    size += 10;
+  }
+}
+buttonCreate.addEventListener("click", () => {
+  const number = input.value;
+  createBoxes(number);
+});
+
+buttonDestroy.addEventListener("click", () => {
+  while (divBoxes.firstChild) {
+    divBoxes.firstChild.remove();
+  }
+});
